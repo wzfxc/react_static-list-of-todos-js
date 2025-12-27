932b3204214bc665fc13c4ee1c4d615733a7f71c
wzfxc <maksohrimenko1@gmail.com>
2025-12-27 20:40:48 +0200

    poxody

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,9 +1,11 @@
 import { UserInfo } from '../UserInfo/UserInfo';
 
 export const TodoInfo = ({ todo }) => (
-  <article className="TodoInfo TodoInfo--completed">
+  <article
+    className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+  >
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
-    <UserInfo />
+    {todo.user ? <UserInfo user={todo.user} /> : ''}
   </article>
 );
